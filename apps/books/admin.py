644064502b9b6905f3a30517a76a6e36e785684f
Jarema Piekutowski <jarema.piekutowski@gmail.com>
2025-06-30 13:16:44 +0200

    optimize admin

--- apps/books/admin.py
+++ apps/books/admin.py
@@ -6,8 +6,13 @@ from .models import Book, BookGenre
 class BookAdmin(admin.ModelAdmin):
     list_display = ('title', 'author', 'date_added', 'user')
     list_filter = ('date_added', 'user')
-    ordering = ('date_added',)
+    ordering = ('-date_added',)
     search_fields = ('title', 'author')
+    list_select_related = ('user', 'genre')
+    list_per_page = 20
+
+    # Optionally, if you have a lot of users, you can use a raw_id_fields for user
+    raw_id_fields = ('user',)
 
 
 # Register your models here.
